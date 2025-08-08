974f5933032402d02da3906936522ee12081e5e7
CodeIsa <isaoliveira782@gmail.com>
2025-08-08 11:43:54 -0300

    adicionando stages ao teste

--- tests/login.test.js
+++ tests/login.test.js
@@ -2,8 +2,15 @@ import http from 'k6/http';
 import { sleep, check } from 'k6';
 
 export const options = {
-  vus: 10, // Number of virtual users
-  duration: '30s', // Duration of the test
+  stages: [
+    { duration: '10s', target: 10 }, // Ramp up to 10 users over 10 seconds
+    { duration: '20s', target: 10 }, // Stay at 10 users for 20 seconds
+    { duration: '10s', target: 30 }, // Ramp down to 30 users over 10 seconds
+    { duration: '20s', target: 30 }, // Stay at 30 users for 20 seconds
+    { duration: '20s', target: 0 }, // Ramp down to 0 users over 20 seconds
+  ],
+  //vus: 10, // Number of virtual users
+  //duration: '30s', // Duration of the test
   thresholds: {
     http_req_duration: ['p(90)<3000', 'max<5000'],
     http_req_failed: ['rate<0.01'],
